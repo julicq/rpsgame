4d28d4e840220d9208fb66856b2a85121166c0ed
julicq <62881010+julicq@users.noreply.github.com>
2020-07-08 16:03:06 +0400

    initial commit

---
+++ rpsgame.py
@@ -0,0 +1,9 @@
+print("------------------------------------")
+print("       ROCK, PAPER & SCISSORS v1")
+print("------------------------------------")
+
+player_1 = input("Enter player 1'' name: ")
+player_2 = input("Enter player 2'' name: ")
+
+rolls = ['rock', 'paper', 'scissors']
+
